093c2322d11e5dc1d466f9fa46fd2c554aa7eaaa
Jose Cambronero <jose.cambronero@cs.nyu.edu>
2016-01-11 14:23:42 -0500

    modified to take directory rather than jre and phosphor jar separately. Added in help. Runs each set of examples with appropriate jre

--- run_examples.sh
+++ run_examples.sh
@@ -3,6 +3,7 @@
 NO_COLOR='\033[0m'
 RED='\033[0;31m'
 GREEN='\033[0;32m'
+USAGE="Usage: ./run_examples.sh <folder-instrumented-jre-and-phosphor-jar>"
 
 function warn {
   printf "$RED $1 $NO_COLOR\n"
@@ -17,22 +18,42 @@ function run_example {
   phosphor_jar=$2
   class=$3
 
-  announce "Running examples from $class"
-  $jre_inst/bin/java -Xbootclasspath/a:$phosphor_jar -javaagent:$phosphor_jar -cp $EXAMPLES_JAR \
-  -ea $class
+  if [ -d $jre_inst ]
+    then
+      announce "Running examples from $class"
+      $jre_inst/bin/java -Xbootclasspath/a:$phosphor_jar -javaagent:$phosphor_jar -cp $EXAMPLES_JAR \
+      -ea $class
+    else
+      warn "Skipping ${class} due to lack of instrumented JRE"
+  fi
  }
 
+function help {
+  echo $USAGE
+  echo -e "The instrumented jres are assumed to follow the following naming convention
+  jre-inst-int: Integer tags
+  jre-inst-obj: Object tags
+  The Phosphor jar is assumed to be of the form find Phosphor*SNAPSHOT.jar, where * is the version
+  Note that these are the names created by mvn verify in the Phosphor project"
+  }
 
-if [ $# != 2 ]
+if [ $# == 1  ] && [ $1 == "-h" ]
   then
-    warn "Usage: ./run_examples.sh <path-instrumented-jre> <path-phosphor-jar>"
+    help
+    exit 0
+fi
+
+if [ $# != 1 ]
+  then
+    help
     exit 1
 fi
 
 # Run tests
-JRE_INST=$1
-PHOSPHOR_JAR=$2
-EXAMPLES_JAR=./target/phosphor-examples-1.0-SNAPSHOT.jar
+JRE_INT=${1}/jre-inst-int
+JRE_OBJ=${1}/jre-inst-obj
+PHOSPHOR_JAR=$(find $1 -iname "Phosphor*SNAPSHOT.jar")
+EXAMPLES_JAR=$(find ./target -iname "phosphor-examples-*SNAPSHOT.jar")
 
 if [ ! -d $JRE_INST ] || [ ! -f $PHOSPHOR_JAR ]
   then
@@ -48,5 +69,7 @@ if [ ! -f $EXAMPLES_JAR ]
 fi
 
 
-run_example $JRE_INST $PHOSPHOR_JAR com.josecambronero.IntegerTagExamples
-#TODO: run_example $JRE_INST $PHOSPHOR_JAR ObjectTagExamples
+run_example $JRE_INT $PHOSPHOR_JAR com.josecambronero.IntegerTagExamples
+run_example $JRE_OBJ $PHOSPHOR_JAR com.josecambronero.ObjectTagExamples
+# TODO automatic
+# TODO implicit flows
---
+++ src/main/java/com/josecambronero/ObjectTagExamples.java
@@ -0,0 +1,7 @@
+package java.com.josecambronero;
+
+/**
+ * Created by josecambronero on 1/11/16.
+ */
+public class ObjectTagExamples {
+}
